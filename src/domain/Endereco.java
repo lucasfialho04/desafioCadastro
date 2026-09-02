98c856e00ea4730f8a327d9559e0a09eebff93db
LUCAS FIALHO DA SILVA <lucasuni90@gmail.com>
2026-09-02 13:14:30 -0300

    adicionando novas validaçoes no cadastro

--- src/domain/Endereco.java
+++ src/domain/Endereco.java
@@ -4,25 +4,34 @@ public class Endereco {
     private String numeroCasa;
     private String cidade;
     private String rua;
-    
+
     public String getNumeroCasa() {
         return numeroCasa;
     }
+
     public void setNumeroCasa(String numeroCasa) {
-        this.numeroCasa = numeroCasa;
+        try {
+            Integer.parseInt(numeroCasa);
+            this.numeroCasa = numeroCasa;
+        } catch (Exception e) {
+            this.numeroCasa = NaoInformado.NAO_INFORMADO;
+        }
     }
+
     public String getCidade() {
         return cidade;
     }
+
     public void setCidade(String cidade) {
         this.cidade = cidade;
     }
+
     public String getRua() {
         return rua;
     }
+
     public void setRua(String rua) {
         this.rua = rua;
     }
 
-    
 }
--- src/service/CadastrarPet.java
+++ src/service/CadastrarPet.java
@@ -3,6 +3,7 @@ package service;
 import java.util.Scanner;
 
 import domain.Endereco;
+import domain.NaoInformado;
 import domain.Pet;
 import domain.Sexo;
 import domain.TipoPet;
@@ -15,15 +16,32 @@ public class CadastrarPet {
         Pet pet1 = new Pet();
 
         // nome
-        pet1.setNome(repostasForm[0]);
+        String regexNome = "^[a-zA-ZÀ-ÿ\\s]+$";
+        boolean nomeValidado = repostasForm[0].matches(regexNome);
+        if (!nomeValidado) {
+            try {
+                throw new TipoInvalidoException("nome invalido");
+            } catch (Exception e) {
+                e.printStackTrace();
+                do {
+                    System.out.println("digite um nome valido");
+                    repostasForm[0] = input.nextLine();
+                    nomeValidado = repostasForm[0].matches(regexNome);
+                } while (!nomeValidado);
+                pet1.setNome(repostasForm[0]);
+            }
+        } else {
+            pet1.setNome(repostasForm[0]);
+        }
 
         // tipo cachorro/gato
+
         if (repostasForm[1].equalsIgnoreCase("cachorro")) {
             pet1.setTipoPet(TipoPet.CACHORRO);
         } else if (repostasForm[1].equalsIgnoreCase("gato")) {
             pet1.setTipoPet(TipoPet.GATO);
         } else {
-            throw new TipoInvalidoException("login invalido");
+            throw new TipoInvalidoException("tipo invalido");
         }
 
         // tipo do sexo
@@ -36,23 +54,43 @@ public class CadastrarPet {
         }
 
         // endreço do pet
+        // cidade
         System.out.println("Qual sua cidade? ");
         repostasForm[3] = input.nextLine();
         Endereco enderecoPet1 = new Endereco();
         enderecoPet1.setCidade(repostasForm[3]);
-
+        // numero
         System.out.println("Qual o numero da casa?");
-        try {
-                String numero = input.nextLine();
-        enderecoPet1.setNumeroCasa(numero);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("numero invalido");
-        }
 
+        String numero = input.nextLine();
+        enderecoPet1.setNumeroCasa(numero);
+
+        // rua
         System.out.println("qual a rua? ");
         enderecoPet1.setRua(input.nextLine());
 
         pet1.setEndereco(enderecoPet1);
+        // idade do pet
+        try {
+            String idade = repostasForm[4];
+            String idadeFinal;
+
+            if (idade.isEmpty()) {
+                idadeFinal = NaoInformado.NAO_INFORMADO;
+            } else {
+                double idadePet = Double.parseDouble(idade);
+                if (idadePet > 20) {
+                    throw new TipoInvalidoException("idade invalida");
+                }
+                if (idadePet < 1) {
+                    idadePet = idadePet / 12;
+                }
+                idadeFinal = String.valueOf(idadePet);
+            }
+
+            pet1.setIdade(idadeFinal);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
